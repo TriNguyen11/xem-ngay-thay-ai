ffaec4942388d3018151c3a7d521ccfab13cbaad
Apple <trinq110902@gmail.com>
2023-10-07 16:31:48 +0700

    kajlhdljkas

--- src/app/localProvider.js
+++ src/app/localProvider.js
@@ -1,9 +1,6 @@
 "use client";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { createContext } from "react";
-
-export const ThemeContext = createContext("");
 
 export function LocalizationProvide({ children }) {
   return (
