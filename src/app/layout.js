f4ef1338b3ab09d9558e86250c6f8627325df2b5
Apple <trinq110902@gmail.com>
2023-10-07 16:27:46 +0700

    asd

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import { LocalizationProvide } from "./LocalProvider";
+import { LocalizationProvide } from "@Root/app/LocalProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
